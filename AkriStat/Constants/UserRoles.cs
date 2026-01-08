a4862731722febdfc0a075de28366c80b96bdb3a
Dean McGuigan <deantmcg@gmail.com>
2026-01-08 19:13:58 +0100

    User helper updates

--- AkriStat/Constants/UserRoles.cs
+++ AkriStat/Constants/UserRoles.cs
@@ -9,5 +9,7 @@ namespace AkriStat.Constants
         public static readonly Guid StandardUserId = new("CE19C27E-AF7C-4D26-B7D2-B733595B91A6");
         public static readonly Guid ScoutId = new("D4F6C614-10F3-4A79-924B-0427F0FE46DD");
         public static readonly Guid ChiefScoutId = new("A6EB7353-483D-4191-A561-8EB97E51E64F");
+
+        public static readonly Guid[] StaffRoles = { AnalystId, ScoutId, ChiefScoutId };
     }
 }
--- AkriStat/Helpers/UserHelper.cs
+++ AkriStat/Helpers/UserHelper.cs
@@ -1,4 +1,5 @@
-﻿using AkriStat.Models;
+﻿using AkriStat.Constants;
+using AkriStat.Models;
 using System;
 using System.Linq;
 
@@ -6,57 +7,24 @@ namespace AkriStat.Helpers
 {
     public static class UserHelper
     {
-        private static readonly Guid AdministratorId = new Guid("649FE9C4-A752-486E-9888-77960D50A003");
-        private static readonly Guid AnalystId = new Guid("B9D0EA79-74E3-4943-A342-494C61F9945C");
-        private static readonly Guid StandardUserId = new Guid("CE19C27E-AF7C-4D26-B7D2-B733595B91A6");
-        private static readonly Guid ScoutId = new Guid("D4F6C614-10F3-4A79-924B-0427F0FE46DD");
-        private static readonly Guid ChiefScoutId = new Guid("A6EB7353-483D-4191-A561-8EB97E51E64F");
-
         public static bool UserIsStaff(this AspNetUsers user)
         {
-            Guid[] staffRoles = new Guid[] { AnalystId, ScoutId, ChiefScoutId };
-
-            if (staffRoles.Any(x => x.Equals(user.GetRoleId())))
-            {
-                return true;
-            }
-
-            return false;
+            return UserRoles.StaffRoles.Any(x => x.Equals(user.GetRoleId()));
         }
 
         public static bool WorksForTeam(this AspNetUsers user)
         {
-            var isStaffRole = user.UserIsStaff();
-
-            if (isStaffRole)
-            {
-                if (user.AspNetUserRoles.First().AspNetUserRoleTeams.Count > 0)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return user.UserIsStaff() && user.AspNetUserRoles.First().AspNetUserRoleTeams.Count > 0;
         }
 
         public static bool IsAdmin(this AspNetUsers user)
         {
-            if (user.GetRoleId().Equals(AdministratorId))
-            {
-                return true;
-            }
-
-            return false;
+            return user.GetRoleId().Equals(UserRoles.AdministratorId);
         }
 
         public static bool IsStandardUser(this AspNetUsers user)
         {
-            if (user.GetRoleId().Equals(StandardUserId))
-            {
-                return true;
-            }
-
-            return false;
+            return user.GetRoleId().Equals(UserRoles.StandardUserId);
         }
 
         public static Guid GetRoleId(this AspNetUsers user)
